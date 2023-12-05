fdc2bef460d97b7cd1ce95241174b31537d0b460
inkdog55 <126940755+inkdog55@users.noreply.github.com>
2023-12-05 12:07:09 +0900

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = process.env.NODE_ENV === "production";
+const repository = "deploy-practice-responsive-web";
+
 const nextConfig = {
-  basePath: "/deploy-practice-responsive-web",
+  //   basePath: "/deploy-practice-responsive-web",
+  assetPrefix: isProduction ? `/${repository}/` : "", // production 일때 prefix 경로
   output: "export",
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
